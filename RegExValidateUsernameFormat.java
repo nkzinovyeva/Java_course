784a38dd280ac75a094d97eedc20b6a91139c611
Nataliya Zinovyeva <52114410+nkzinovyeva@users.noreply.github.com>
2020-04-14 00:10:10 +0300

    Validates the username format

---
+++ RegExValidateUsernameFormat.java
@@ -0,0 +1,21 @@
+
+
+import java.util.Scanner;
+
+public class RegExValidateUsernameFormat {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter username: ");
+		String username = input.nextLine();
+
+		if (username.matches("[a]{1}[0-9]{7}(@myy\\.haaga-helia\\.fi)") == true) {
+			System.out.println("OK");
+		} else {
+			System.out.println("Invalid username format");
+		}
+		input.close();
+	}
+
+}
